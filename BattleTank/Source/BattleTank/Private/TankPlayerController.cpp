4154c84f51fbad1492e27f4545ed8f7e467b0a23
MikeJPalmer <gamingchamptv@gmail.com>
2020-06-01 22:34:36 -0400

    Added Tick method so that we can build in aiming from the controlled tank to the world.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -19,7 +19,24 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {	
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	// Get World location through the crosshair.
+	// If it hits the landscape
+		// Tell controlled tank to aim at this point.
+
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -15,7 +15,12 @@ UCLASS() class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
+	void BeginPlay() override;
+	void Tick(float DeltaTime) override;
+
 	ATank* GetControlledTank() const;
 
-	void BeginPlay() override;
+	// Start the tank moving the barrel so that a shot would hit where
+	// the crosshair intersects with the world.
+	void AimTowardsCrosshair();
 };
