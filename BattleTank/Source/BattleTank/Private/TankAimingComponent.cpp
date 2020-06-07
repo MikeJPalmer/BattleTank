2b04b5cf0cf48cc5ea7a14738aeed4d92242a69e
MikeJPalmer <gamingchamptv@gmail.com>
2020-06-07 17:40:18 -0400

    Performed some code cleanup, removing Tick from components we don't need it, rearranging the logging so that we can determine there's a bug with the aim solution code, it's not finding a solution every frame which will need to be addressed.  PS: UE bug confirmed (on my end anyways) the bank static mesh keeps being removed from the C++ StaticMesh Component each time UE is closed (or in this case it crashes on me and I don't see it on the reload).

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -9,7 +9,7 @@ UTankAimingComponent::UTankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = true; //TODO should this really tick?
 
 	// ...
 }
@@ -44,6 +44,13 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	{
 		FVector AimDirection = OutLaunchVelocity.GetSafeNormal();
 		MoveBarrelTowards(AimDirection);
+		float Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found!"), Time);
+	}
+	else
+	{
+		float Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f: No solution found!"), Time);
 	}
 	// if no solution found do nothing
 }
